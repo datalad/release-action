3fc6067b1614e8d902fcf5c08f02e02204c5d0e1
John T. Wodder II <git@varonathe.org>
2022-10-04 10:08:05 -0400

    Generalize mkchlog-snippet.py

---
+++ datalad_release_action/__main__.py
@@ -0,0 +1,54 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+import os
+from typing import IO
+
+import click
+
+from .config import Config
+from .client import Client
+
+
+@dataclass
+class DRA:
+    config: Config
+    client: Client
+
+
+@click.group()
+@click.option(
+    "-c", "--config", type=click.File("r"), default=".datalad-release-action.yaml"
+)
+@click.pass_context
+def main(ctx: click.Context, config: IO[str]) -> None:
+    cfg = Config.load_yaml(config)
+    try:
+        token = os.environ["GITHUB_TOKEN"]
+    except KeyError:
+        raise click.UsageError("GITHUB_TOKEN envvar not set")
+    client = ctx.with_resource(Client(token))
+    ctx.obj = DRA(config=cfg, client=client)
+
+
+@main.command()
+@click.argument("repo_owner")
+@click.argument("repo_name")
+@click.argument("prnum", type=int)
+@click.pass_obj
+def add_changelog_snippet(
+    dra: DRA, repo_owner: str, repo_name: str, prnum: int
+) -> None:
+    pr = dra.client.get_pr_info(
+        repo_owner=repo_owner,
+        repo_name=repo_name,
+        prnum=prnum,
+    )
+    dra.config.snippets_dir.mkdir(parents=True, exist_ok=True)
+    outfile = dra.config.snippets_dir / f"pr-{prnum}.md"
+    outfile.write_text(pr.as_snippet(dra.config))
+    print("Changelog snippet saved to", outfile)
+
+
+if __name__ == "__main__":
+    main()
---
+++ datalad_release_action/config.py
@@ -0,0 +1,40 @@
+from __future__ import annotations
+
+from enum import Enum
+from pathlib import Path
+from typing import List, IO, Optional
+
+from pydantic import BaseModel, Field
+from ruamel.yaml import YAML
+
+
+class GitAuthor(BaseModel):
+    name: str = "DataLad Bot"
+    email: str = "bot@datalad.org"
+
+
+class Bump(Enum):
+    MAJOR = "major"
+    MINOR = "minor"
+    PATCH = "patch"
+
+
+class Category(BaseModel):
+    name: str
+    bump: Bump = Bump.PATCH
+    label: Optional[str] = None
+
+
+class Config(BaseModel):
+    git_author: GitAuthor = Field(default_factory=GitAuthor)
+
+    ### apt_depends
+    ### pip_depends
+    ### pre_tag
+
+    snippets_dir: Path = Path("changelog.d")
+    categories: List[Category]
+
+    @classmethod
+    def load_yaml(cls, infile: IO[str]) -> Config:
+        return cls.parse_obj(YAML(typ="safe").load(infile))
