b2a0713c0c55fe0b8e5be0cb7c707b837f2abb67
John T. Wodder II <git@varonathe.org>
2022-10-05 09:19:02 -0400

    Handle PWD issues

--- noxfile.py
+++
@@ -1,9 +0,0 @@
-import nox
-
-nox.options.reuse_existing_virtualenvs = True
-
-
-@nox.session
-def run(session):
-    session.install("-r", "requirements.txt")
-    session.run("python", "-m", "datalad_release_action", *session.posargs)
